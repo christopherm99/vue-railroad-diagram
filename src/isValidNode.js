32f518194f3d0afd07db9602d81e8f3b67a7637f
Christopher Milan <chris.m.milan@gmail.com>
2019-06-17 12:58:23 -0400

    Setup scaffolding for vue component
    Write validation tests

---
+++ src/isValidNode.js
@@ -0,0 +1,59 @@
+function isBranch(node) {
+  switch (node.type) {
+    case "choice":
+      return Array.isArray(node.children) &&
+        node.children.every(child => isNode(child)) &&
+        node.default
+        ? typeof node.default === "number" &&
+            node.default < node.children.length - 1
+        : true;
+    case "optional":
+      return (
+        (node.children
+          ? Array.isArray(node.children) &&
+            node.children.every(child => isNode(child))
+          : node.child && isNode(node.child)) &&
+        typeof node.normal === "boolean"
+      );
+    case "multiple":
+      return (node.children
+        ? Array.isArray(node.children) &&
+          node.children.every(child => isNode(child))
+        : node.child && isNode(node.child)) &&
+        (node.repeat !== undefined ? typeof node.normal === "boolean" : true) &&
+        typeof node.optional === "boolean" &&
+        node.optional
+        ? typeof node.normal === "boolean"
+        : true;
+    default:
+      return false;
+  }
+}
+
+function isTextLeaf(node) {
+  return (
+    typeof node.text === "string" &&
+    (typeof node.href === "string" || !node.href)
+  );
+}
+
+function isLeaf(node) {
+  switch (node.type) {
+    case "terminal":
+      return isTextLeaf(node);
+    case "nonTerminal":
+      return isTextLeaf(node);
+    case "comment":
+      return isTextLeaf(node);
+    case "skip":
+      return true;
+    default:
+      return false;
+  }
+}
+
+function isNode(node) {
+  return isBranch(node) || isLeaf(node);
+}
+
+export default isNode;
--- src/vue-railroad-diagram.vue
+++ src/vue-railroad-diagram.vue
@@ -1,35 +1,83 @@
-<script>
-export default {
-    name: 'VueRailroadDiagram', // vue component name
-    data() {
-        return {
-            counter: 5,
-            initCounter: 5,
-        };
-    },
-}
-</script>
-
 <template>
-    <div class="vue-railroad-diagram">
-        <p>The counter is set to <b>{{counter}}</b>.</p>
-        <button @click="counter += 1">Click +1</button>
-        <button @click="counter -= 1">Click -1</button>
-        <button @click="counter = initCounter">Reset</button>
-    </div>
+  <div class="vue-railroad-diagram">
+    <p>
+      The counter is set to <b>{{ counter }}</b
+      >.
+    </p>
+    <button @click="counter += 1">Click +1</button>
+    <button @click="counter -= 1">Click -1</button>
+    <button @click="counter = initCounter">Reset</button>
+  </div>
 </template>
 
+<script>
+import {
+  Diagram,
+  ComplexDiagram,
+  Sequence,
+  Choice,
+  Optional,
+  OneOrMore,
+  ZeroOrMore,
+  Terminal,
+  NonTerminal,
+  Comment,
+  Skip
+} from "railroad-diagrams";
+import isValidNode from "./isValidNode";
+
+export default {
+  name: "VueRailroadDiagram", // vue component name
+  data() {
+    return {
+      counter: 5,
+      initCounter: 5
+    };
+  },
+  created() {
+    console.log(Diagram(Terminal("+"), NonTerminal("Number")).toString());
+  },
+  props: {
+    grammar: {
+      type: Array,
+      validator(value) {
+        return value.every(step => {
+          isValidNode(step);
+        });
+      }
+    },
+    options: {
+      type: Object,
+      default: function() {
+        return {
+          isStack: false,
+          isComplex: false
+        };
+      },
+      validator(value) {
+        return (value.isStack !== undefined
+          ? typeof value.isStack === "boolean"
+          : true) && value.isComplex !== undefined
+          ? typeof value.isComplex === "boolean"
+          : true;
+      },
+      required: false
+    }
+  }
+};
+</script>
+
 <style scoped>
-    .vue-railroad-diagram {
-        display: block;
-        width: 400px;
-        margin: 25px auto;
-        border: 1px solid #ccc;
-        background: #eaeaea;
-        text-align: center;
-        padding: 25px;
-    }
-    .vue-railroad-diagram p {
-        margin: 0 0 1em;
-    }
+.vue-railroad-diagram {
+  display: block;
+  width: 400px;
+  margin: 25px auto;
+  border: 1px solid #ccc;
+  background: #eaeaea;
+  text-align: center;
+  padding: 25px;
+}
+.vue-railroad-diagram p {
+  margin: 0 0 1em;
+}
 </style>
